f402722187641b7aa8d112a865e50b6279cfbc72
Maldi2910 <malevich_diana15@mail.ru>
2022-08-06 14:50:46 +0300

    1-getRequest+Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type ButtonTypeProps = {
+    nickName: string,
+    callBack: ()=>void
+}
+
+export const Button = (props: ButtonTypeProps) => {
+
+const onClickButtonHandler =()=> {
+    props.callBack()
+}
+
+    return (
+
+            <button onClick={onClickButtonHandler}>{props.nickName}</button>
+    )
+}
